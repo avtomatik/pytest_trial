f5b41f6c220f307d81f7f61048c112bee388eb7d
Alexander <kkruscherr@gmail.com>
2024-06-16 22:57:23 +0300

    Up to `Библиотека pytest`, 05 Theory Over

--- test_engine.py
+++ test_engine.py
@@ -1,20 +1,13 @@
-import pytest
-
 from engine_class import Engine
 
 
-@pytest.fixture
-def engine():
-    """Фикстура возвращает экземпляр класса двигателя."""
-    return Engine()
-
-
-@pytest.fixture
-def start_engine(engine):
-    """Фикстура запускает двигатель."""
-    engine.is_running = True
-
-
-def test_engine_is_running(engine, start_engine):
+def test_engine_is_running(engine):
     """Тест проверяет, работает ли двигатель."""
+    print('test_engine_is_running')
     assert engine.is_running
+
+
+def test_check_engine_class(engine):
+    """Тест проверяет класс объекта."""
+    print('test_check_engine_class')
+    assert isinstance(engine, Engine)
